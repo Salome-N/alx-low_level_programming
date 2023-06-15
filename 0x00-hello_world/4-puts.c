1c601953f71d0a783c7682f3c84fed1998cb82aa
Salome-N <njengasalome87@gmail.com>
2023-06-15 07:20:18 -0700

    Fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+* main - entry point
+* return value of 'main' - always returns zero
+*/  
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
