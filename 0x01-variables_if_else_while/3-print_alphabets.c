112128c88d04e81113cbbe99b0dfd9456c889dc2
Salome-N <njengasalome87@gmail.com>
2023-06-16 00:27:55 -0700

    New file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+* main - entry point
+* @AtoZ - alphabet starting point 'A'
+* Description: print out the alphabet in
+* lowercase then uppercase from A-Z
+* Return: always return 0
+*/
+int main(void)
+{
+	for (int AtoZ = 'A'; AtoZ <= 'Z'; AtoZ++)
+	{
+		putchar(tolower(AtoZ));
+	}
+	for (int AtoZ = 'A'; AtoZ <= 'Z'; AtoZ++)
+	{
+		putchar(AtoZ);
+	}
+	putchar('\n');
+	return (0);
+}
