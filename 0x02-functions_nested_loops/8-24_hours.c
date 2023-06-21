611be1c487c06ca1660c09434613ca4faa8b406a
Salome-N <njengasalome87@gmail.com>
2023-06-21 16:15:32 -0700

    fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 /**
 * jack_bauer - print 24 hour
+*
 * Description: print out the 24 hour clock
 * @a: holds the hour side
 * @b: holds the hour side
 * @c: holds the minute side
 * @d: holds the minute side
+*
 * Return: always 0
 */
 void jack_bauer(void)
