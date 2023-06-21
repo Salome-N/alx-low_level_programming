1acd933fd4c3d4797d0b151ee0d2eeaa1d631a6a
Salome-N <njengasalome87@gmail.com>
2023-06-21 08:06:49 -0700

    New file

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* print_alphabet - function to print out the alphabet
+* Return: always 0
+*/
+void print_alphabet(void);
+{
+	char az;
+	for ( az = 'a'; az <= 'z'; az++)
+		print_alphabet(az);
+	_putchar('\n');
+	return (0);
+}
