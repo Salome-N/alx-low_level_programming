a0ece31111f378a6ecc85f179a7f9671b90507c0
Salome-N <njengasalome87@gmail.com>
2023-06-21 15:36:17 -0700

    fix

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,17 +2,14 @@
 /**
 * _abs - checks the absolute value of a number
 * @i: holds the value being checked
-* @i_val: holds the negative sign value
 * Return: i if the value is more than zero,i_val else otherwise
 */
 int _abs(int i)
 {
 	if (i < 0)
 	{
-		int i_val;
-
-		i_val = i * -1;
-		return (i_val);
+		i = i * -1;
+		return (i);
 	}
 	return (i);
 }
