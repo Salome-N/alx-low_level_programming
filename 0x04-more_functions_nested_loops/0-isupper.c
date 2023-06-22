d0277e85621c1e8b06f83cde1da17d72e5cd2af1
Salome-N <njengasalome87@gmail.com>
2023-06-22 08:57:05 -0700

    Fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
 * _isupper - returns value if it is uppercase or otherwise
 * @c - holds value
 * Return: 1 if uppercase 0 if otherwise
 */
+
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
