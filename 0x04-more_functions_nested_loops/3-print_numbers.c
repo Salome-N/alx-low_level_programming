e74ffeb14a88a01ce5f11f11bb49b34df2043562
Salome-N <njengasalome87@gmail.com>
2023-06-23 02:26:55 -0700

    New file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_numbers - prints out numbers
+* Return: always 0
+*/
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
+
+	_putchar("$\n");
+
+	return (0);
+}
