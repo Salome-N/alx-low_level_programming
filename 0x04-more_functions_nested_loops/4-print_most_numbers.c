285a54f37c556e7921f0df561bae37213b501827
Salome-N <njengasalome87@gmail.com>
2023-06-23 02:43:58 -0700

    New file

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+* print_most_numbers - print out numbers apart from 2 and 4
+* Return: always 0
+*/
+
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n == 2 && n == 4)
+		{
+			continue;
+		}
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+}
