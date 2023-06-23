c0c92c49fadc7b1921f9189c2bd07a17aac7164f
Salome-N <njengasalome87@gmail.com>
2023-06-23 02:19:37 -0700

    New file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* mul - calculates the multiple of a and b
+* @a: holds value
+* @b: holds value
+* Return: returns the multiple value
+*/
+
+int mul(int a, int b)
+{
+	int a_mul;
+
+	a_mul = a * b;
+
+	return (a_mul);
+}
