592ba8505dbcd485f8adbec6ce830d348a85e4f6
Salome-N <njengasalome87@gmail.com>
2023-06-26 18:17:27 -0700

    New file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+* print_rev - prints reverse
+* @s: holds string
+* Return: always 0
+*/
+
+void print_rev(char *s)
+{
+	int a, l;
+
+	a = 0;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	
+	for (l = a - 1; l >= 0; l--)
+	{
+		_putchar(s[l]);
+	}
+
+	_putchar('\n');
+}
