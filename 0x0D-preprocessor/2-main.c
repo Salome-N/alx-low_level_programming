cbecec364e023216329cb021d961850343609232
Salome-N <njengasalome87@gmail.com>
2023-07-18 13:44:09 -0700

    Fix

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
-* main - prints the name of the file it was compiled from, followed by a new line
-* @Return: always 0
+* main - prints the name of the file followed by a new line
+* Return: always 0
 */
 
 int main(void)
