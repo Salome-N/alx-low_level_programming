2998ae27c777276be5ba531e22a32f7b49e57400
Salome-N <njengasalome87@gmail.com>
2023-07-18 05:36:16 -0700

    New directory and files

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - holds name of the dog, age and owner
+* @name: name of the dog
+* @age: age of the dog
+* @owner: name of the owner of the dog
+*
+* Description: details about a dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char owner;
+};
+
+#endif
