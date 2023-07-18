cec84852b2a66c16085e935bb21c8785437794b4
Salome-N <njengasalome87@gmail.com>
2023-07-18 03:48:34 -0700

    New file and fix

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,1 +1,5 @@
+/**
+* macro named SIZE as an abbreviation for the token 1024
+*/
+
 #define SIZE 1024
---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,5 @@
+/**
+* macro named PI as an abbreviation for the token 3.14159265359
+*/
+
+#define PI 3.14159265359
