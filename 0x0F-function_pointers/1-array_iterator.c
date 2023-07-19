679e9e596592b55d5aa405f6222e40f5644f779f
Salome-N <njengasalome87@gmail.com>
2023-07-19 12:39:24 -0700

    New directory and files

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+
+/**
+* array_iterator - executes function given as parameter on each elemnt of array
+* @array: an array
+* @size: size of array
+* @action: pointer to array
+* Return: 0 since void
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return (NULL);
+
+	while (size > 0)
+	{
+		action(*array);
+		array++, size--;
+	}
+}
