1c1b7c77d735bd2a95550e41c4d7b4618bc077be
Salome-N <salomenjenga87@gmail.com>
2023-07-27 11:05:07 -0700

    New files

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+#include <string.h>
 
 /**
 * add_node - adds a new node at the beginning of a list_t list
@@ -19,7 +20,6 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	d = strup(str);
-
 	if (d == NULL)
 	{
 		free(new);
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* free_list - free list_t list
+* @head: ptr to list_t list
+* Return: 0
+*/
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp
+	}
+}
