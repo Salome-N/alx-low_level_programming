016d0d7b691f02aae4fcb9c6871bce1485fd9169
Salome-N <salomenjenga87@gmail.com>
2023-08-01 17:16:22 -0700

    Update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -74,7 +74,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new->name = strc(new-name, name);
+	new->name = strc(new->name, name);
 	new->age = age;
 	new->owner = strc(new->owner, owner);
 
