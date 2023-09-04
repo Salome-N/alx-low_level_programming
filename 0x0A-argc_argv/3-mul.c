bb4609075acf7fa522c2253a35bb46c845cd409a
Salome-N <njengasalome87@gmail.com>
2023-09-04 11:14:34 -0700

    New file

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - multiplies two numbers
+* @argc: no. of arguments
+* @argv: array of ptr to arguments
+* Return: zero
+*/
+
+int main(int argc, char *argv[])
+{
+	int n1, n2, mul;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	mul = n1 * n2;
+
+	printf("%d\n", mul);
+
+	return (0);
+}
