b220570e5bfa52059b1d737869924a61d59d0867
Salome-N <njengasalome87@gmail.com>
2023-09-11 01:38:00 -0700

    New directory and files

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - prints the name of the file it was compiled and a new line
+* Return: name of file
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
