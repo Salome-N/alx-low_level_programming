19b58ac65e07165610c4d89887f4e819a308268d
Salome-N <njengasalome87@gmail.com>
2023-09-21 12:16:00 -0700

    Assembly Lang

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+section .text
+	extern printf
+	global main
+
+main:
+	push rbp
+
+	mov rdi, format
+	mov rsi, message
+	mov rax, 0
+
+	call printf
+	pop rbp
+
+	mov rax, 0
+	ret
+
+section .data
+	message: db "Hello, Holberton", 0
+	format: db "%s", 10, 0
