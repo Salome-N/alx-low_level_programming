d7b7d3c92ee26922bf316b57e99528e74a9d149c
Salome-N <njengasalome87@gmail.com>
2023-09-26 01:26:05 -0700

    New files

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+* print_listint - prints all the elements of a listint_t list
+* @h: listint list
+* Return: no. of nodes
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t modes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+
+	return (nodes);
+}
