20c54db8a53aa0a2f098beb05af049f9053145bf
Salome-N <njengasalome87@gmail.com>
2023-09-28 06:47:39 -0700

    Binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,12 +9,12 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int n = 0, m = 1;
-	int l = 0;
+	int l;
 
 	if (b == '\0')
 		return (0);
 
-	while (b[l])
+	for (l = 0; b[l];)
 		l++;
 
 	for (l -= 1; l >= 0; l--)
