76f034f4e0f287634b40ac142dbd4eea77ce0bf1
Salome-N <njengasalome87@gmail.com>
2023-12-01 11:12:22 -0800

    Fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -16,7 +16,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	htable->size = size;
-	htable->array = malloc(sizeof(hash_table_t hash_table_t *) * size);
+	htable->array = malloc(sizeof(hash_table_t *) * size);
 	if (htable->array == NULL)
 		return (NULL);
 
